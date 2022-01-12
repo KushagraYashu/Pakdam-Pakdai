3ab407a7ec0ad208430925403a0d47466d07f0f2
Kushagra <syashu31@gmail.com>
2022-01-13 01:37:02 +0530

    Full Player Movement with jump and Speed Ability is Set up. 13th Jan 2022 01:36AM

--- Assets/Scripts/PlayerMovementPlayer1.cs
+++ Assets/Scripts/PlayerMovementPlayer1.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerMovementPlayer1 : MonoBehaviour
 {
     public float speed = 5f;
-    
+    public float jumpStrength = 5f;
+    public bool isJumping = false;
+    public float jumps;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +21,34 @@ public class PlayerMovementPlayer1 : MonoBehaviour
         float h = Input.GetAxis("Horizontal Player 1");
         float v = Input.GetAxis("Vertical Player 1");
         transform.Translate(new Vector3(h, 0, v) * speed * Time.deltaTime);
+        if (Input.GetKeyDown("space"))
+        {
+            if (!isJumping)
+            {
+                print("Jump");
+                jumps++;
+                isJumping = true;
+                gameObject.GetComponent<Rigidbody>().velocity = transform.up * jumpStrength;
+            }
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
         print(collision.transform.tag);
+        if (collision.transform.tag == "ground")
+        {
+            isJumping = false;
+            jumps = 0;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag == "ground")
+        {
+            isJumping = true;
+            jumps++;
+        }
     }
 }
