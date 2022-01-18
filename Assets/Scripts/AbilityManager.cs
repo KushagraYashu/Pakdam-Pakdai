bccb8ef8c2723026b29db39013ab5d00c65dc5f5
Kushagra <syashu31@gmail.com>
2022-01-19 00:32:30 +0530

    basic level design. 19th Jan 2022 12:32AM

--- Assets/Scripts/AbilityManager.cs
+++ Assets/Scripts/AbilityManager.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class AbilityManager : MonoBehaviour
 {
     public GameObject speedAbilityPrefab;
+    public GameObject[] speedAbilitySpawnPoints; 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        speedAbilitySpawnPoints = GameObject.FindGameObjectsWithTag("Speed Ability Spawn Point");
     }
 
     // Update is called once per frame
